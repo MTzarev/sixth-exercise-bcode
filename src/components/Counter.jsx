3abb4ec43803a1ee8e24710ca27f09c5b30c8eaa
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-25 09:19:58 +0200

    use Boolean to setState

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,13 +4,12 @@ function Counter () {
     const [name, setName]= useState("")
     const [isStudent, setIsStudent] = useState(false)
 
+
     const buttonClick = () =>{
         setCount(count +1)
         setName("Liolio")
     }
-    const studentClick = () =>{
-        setIsStudent(!isStudent)
-    }
+   
     const decrement = () => {
         setCount(count - 1)
     }
@@ -20,22 +19,25 @@ function Counter () {
     const increment = () => {
         setCount(count+1)
     }
+    const studentInfo = () => {
+        setIsStudent(!isStudent)
+    }
     return (
         <>
         <p>Name: {name}</p>
         <button onClick={buttonClick}>Clicker Button</button>
         <p>Count + {count}</p>
         <hr />
-        <p>{isStudent ?"Yes":"No"}</p>
-        <button onClick={studentClick} className='student-button'>Are you a student? </button>
-        <hr />
+       
         <p>{count}</p>
             <div>
         <button onClick={increment}>+</button>
         <button onClick={reset}>Reset</button>
         <button onClick={decrement}>-</button>
-        
         </div>
+        <hr />
+        <p>{isStudent?"Yes":"No"}</p>
+        <button onClick={studentInfo}>Student Info</button>
         </>
     );
 
